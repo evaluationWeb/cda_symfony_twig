26873b98b18eaba1f8373f440b8e9e2d7edea3f8
mithridatem <mathieumithridate@adrar-formation.com>
2025-03-06 17:16:26 +0100

    feat : api addArticle

--- src/Controller/ApiArticleController.php
+++ src/Controller/ApiArticleController.php
@@ -7,13 +7,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use App\Repository\AccountRepository;
+use App\Repository\CategoryRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 
 final class ApiArticleController extends AbstractController
 {
 
     public function __construct(
-        private ArticleRepository $articleRepository
+        private readonly ArticleRepository $articleRepository,
+        private readonly AccountRepository $accountRepository,
+        private readonly CategoryRepository $categoryRepository,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly SerializerInterface $serializer,
     ) {}
 
 
@@ -44,6 +53,47 @@ final class ApiArticleController extends AbstractController
             $code = 404;
         }
         //retourner la réponse json
+        return $this->json(
+            $article,
+            $code,
+            [
+                "Access-Control-Allow-Origin" => "*",
+                "Content-Type" => "application/json"
+            ],
+            ['groups' => 'articles:read']
+        );
+    }
+
+
+    #[Route('/api/article', name: 'api_article_add', methods: ['POST'])]
+    public function addArticle(Request $request): Response
+    {
+        $json = $request->getContent();
+        $article = $this->serializer->deserialize($json, Article::class, 'json');
+        //test si les champs sont remplis
+        if ($article->getTitle() && $article->getContent() && $article->getAuthor()) {
+            //récupération du compte
+            $article->setAuthor($this->accountRepository->findOneBy(["email" => $article->getAuthor()->getEmail()]));
+            //Récupération des catégories
+            foreach ($article->getCategories() as $key => $value) {
+                $cat = $value->getName();
+                $article->removeCategory($value);
+                $cat = $this->categoryRepository->findOneBy(["name" => $cat]);
+                $article->addCategory($cat);
+            }
+            //Test l'article n'existe pas
+            if (!$this->articleRepository->findOneBy(["title" => $article->getTitle(), "content" => $article->getContent()])) {
+                $this->entityManager->persist($article);
+                $this->entityManager->flush();
+                $code = 201;
+            } else {
+                $code = 400;
+                $article = "Article existe déjà";
+            }
+        } else {
+            $code = 400;
+            $article = "Champs non remplis";
+        }
         return $this->json(
             $article,
             $code,
