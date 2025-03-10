6fb543a4ac6511a25062757e4ba20e83ce4c8153
mithridatem <mathieumithridate@adrar-formation.com>
2025-03-10 17:00:36 +0100

    feat : correction add Account

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,13 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\AccountRepository;
+use App\Entity\Account;
+use App\Form\AccountType;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 final class UserController extends AbstractController
 {
     public function __construct(
-        private readonly AccountRepository $accountRepository
+        private readonly AccountRepository $accountRepository,
+        private readonly EntityManagerInterface $em
     ) {}
 
     #[Route('/register', name: 'app_user_register')]
@@ -30,6 +36,42 @@ final class UserController extends AbstractController
     {
         return $this->render('user/accounts.html.twig', [
             "accounts" => $this->accountRepository->findAll()
+        ]);
+    }
+
+    #[Route('/account/add', name:'app_account_add')]
+    public function addAccount(Request $request): Response
+    {
+
+        $user = new Account();
+        $form = $this->createForm(AccountType::class, $user);
+        $form->handleRequest($request);
+        $type = "";
+        $msg = "";
+        //test si le formulaire est submit
+        if($form->isSubmitted() && $form->isValid()) {
+            try {
+                //Test si le compte n'existe pas
+                if(!$this->accountRepository->findOneBy(['email' => $user->getEmail()])) {
+                    $user->setRoles('ROLE_USER');
+                    $this->em->persist($user);
+                    $this->em->flush();
+                    $type = "success";
+                    $msg = "Compte ajouté avec succès";
+                }
+                //Sinon si le compte
+                else {
+                    $type = "danger";
+                    $msg = "Email déjà utilisé";
+                }
+            }catch(\Exception $e) {
+                $type = "danger";
+                $msg="Enregistrement échoué";
+            }
+        $this->addFlash($type, $msg);
+        }
+        return $this->render('user/addaccount.html.twig',[
+            'formulaire' =>$form
         ]);
     }
 }
